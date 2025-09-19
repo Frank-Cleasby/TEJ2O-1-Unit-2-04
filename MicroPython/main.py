43730ff2f1294637e75b2e6da273602f23de4275
Frank-Cleasby <frank.cleasby@stu.ocsb.ca>
2025-09-19 17:30:26 +0000

    temperature

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,24 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Frank
+Created on: Sep 2025
+This module declares the temperature
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+# the variable for temperature
+temperature = 0
+
+display.clear()
+display.show(Image.HAPPY)
+
+
+while True:
+    if button_a.is_pressed() :
+        temperature = temperature()
+        display.show(str('The temperature is:'))
+        display.show(str(temperature))
+        display.show(str('C.'))
+        display.clear()
+        display.show(Image.HAPPY)
